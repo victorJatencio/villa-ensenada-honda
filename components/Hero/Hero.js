3e3006c5ddb2776984a0041747bfe908f4ed971d
victor atencio <victorj.atencio@gmail.com>
2021-04-05 22:21:49 -0400

    added tailwind

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -1,10 +1,11 @@
 
-
 const Hero = () => {
   return (
-    <div>
-      <h1>Villa Ensenada Honda</h1>
-      <p>Culebra, Puerto Rico</p>
+    <div className="jumbotron px-4">
+      <div className="container mx-auto">
+        <h1>Villa Ensenada Honda</h1>
+        <p>Culebra, Puerto Rico</p>
+      </div>
     </div>
   )
 }
